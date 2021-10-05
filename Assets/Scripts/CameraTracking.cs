acf5e0c2ef9ece6472ab9d387205251ec035da46
benjaminliang2 <benjaminliang2@gmail.com>
2021-10-05 11:03:20 -1000

    Camera Tracking Working
    Camera doesnt track y-axis when player exceeds y position threshold.

--- Assets/Scripts/CameraTracking.cs
+++ Assets/Scripts/CameraTracking.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,44 @@ using UnityEngine;
 public class CameraTracking : MonoBehaviour
 {
     GameObject objectToTrack;
-    Transform cameraLock;
+    GameObject camTrigger;
+    Vector3 tempTransform;
     void Start()
     {
         objectToTrack = GameObject.FindGameObjectWithTag("PlayerRight");
+        camTrigger = GameObject.FindGameObjectWithTag("CameraTrigger");
         transform.position = objectToTrack.transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-            transform.position = new Vector2(objectToTrack.transform.position.x,
-                                          objectToTrack.transform.position.y);
+        //transform.position = new Vector2(objectToTrack.transform.position.x,
+        //                                objectToTrack.transform.position.y);
+        Debug.LogError(transform.position);
 
+        if (camTrigger.GetComponent<CameraTrigger>().cameraYaxisLock)
+        {
+            TrackX();
+        }
+        else
+        {
+            TrackY();
+            TrackX();
+        }
+
+    }
+
+    private void TrackX()
+    {
+        tempTransform.x = (objectToTrack.transform.position.x);
+        transform.position = tempTransform;
+    }
+
+    private void TrackY()
+    {
+        tempTransform.y = (objectToTrack.transform.position.y);
+        transform.position = tempTransform;
     }
 }
